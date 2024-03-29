9834eb439c4ab03df8a89f970a6bf547e8ea1a4b
syoyuda <yyoshida834@gmail.com>
2024-03-29 08:07:48 +0000

    init

---
+++ src/odom.cpp
@@ -0,0 +1,134 @@
+// ファイル名　my_odom3.cpp
+#include <ros/ros.h>  // rosで必要はヘッダーファイル
+#include <geometry_msgs/Twist.h> // ロボットを動かすために必要
+#include <nav_msgs/Odometry.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_datatypes.h>
+#include <gazebo_msgs/ModelStates.h> 
+#include <sensor_msgs/JointState.h>
+#include <iostream>
+
+
+
+using namespace std;
+ros::Publisher G_publisher_odom;
+nav_msgs::Odometry G_odom;
+ros::Time G_wall_begin ;
+
+// コールバック関数。並進、回転速度の表示。
+void cbVel(const geometry_msgs::Twist::ConstPtr& vel) {
+  cout << "Linear :" << vel->linear.x << endl;
+  cout << "Angular:" << vel->angular.z << endl;
+}
+
+
+// /odomトピックから位置posと姿勢poseを表示
+void cbOdom(const nav_msgs::Odometry::ConstPtr& msg)
+{
+  ROS_INFO("Seq: %d", msg->header.seq);
+  ROS_INFO("/odom Pos (x:%f, y:%f, z:%f)", msg->pose.pose.position.x,msg->pose.pose.position.y, msg->pose.pose.position.z);
+
+  tf::Quaternion q(msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);  
+  // tf::Quaternion q(quat.x, quat.y, quat.z, quat.w);
+  tf::Matrix3x3 m(q);
+  double roll, pitch, yaw;
+  m.getRPY(roll, pitch, yaw);
+
+  ROS_INFO("/odom Pose (roll:%f, pitch:%f, yaw:%f) ", roll, pitch, yaw);
+  ROS_INFO("Vel (Linear:%f, Angular:%f)", msg->twist.twist.linear.x,msg->twist.twist.angular.z);
+              
+}
+
+// /gazebo/model_statesトピックから真の位置Pos(x,y,z)と姿勢Pose(roll, pitch ,yaw)を表示
+void cbModelStates(const gazebo_msgs::ModelStates::ConstPtr& msg)
+{
+  ROS_INFO("Real Pos (x:%f, y:%f, z:%f)", msg->pose[1].position.x,msg->pose[1].position.y, msg->pose[1].position.z);
+
+  tf::Quaternion q(msg->pose[1].orientation.x, msg->pose[1].orientation.y, msg->pose[1].orientation.z, msg->pose[1].orientation.w);  
+  // tf::Quaternion q(quat.x, quat.y, quat.z, quat.w);
+  tf::Matrix3x3 m(q);
+  double roll, pitch, yaw;
+  m.getRPY(roll, pitch, yaw);
+
+  ROS_INFO("Real Pose (roll:%f, pitch:%f, yaw:%f) ", roll, pitch, yaw);
+              
+}
+
+// cbMyOdom：この関数に自分のオドメトリを実装しよう！
+// /joint_statesトピックから左右のjoint(車輪回転軸)の位置（回転角度)[rad]を表示
+// 参考：Turtlebot3の車輪直径0.066 [m]
+void cbMyOdom(const sensor_msgs::JointState::ConstPtr& jointstate)
+{
+  double wheel_right_joint_pos = jointstate->position[0]; // 右車軸の位置[rad]
+  double wheel_left_joint_pos  = jointstate->position[1]; // 左車軸の位置[rad]
+
+   //自分で変更したところ
+  double nt, positionr, positionl, positionrb, positionlb, velr, vell, vel, positionx, positiony, s, X, Y, Vx, Vy, W;
+  ros::Time wall_now = ros::Time::now();
+  
+
+  if(wall_now.toSec() <= 0){
+    positionrb = jointstate->position[0];
+    positionlb = jointstate->position[1];
+  }else{
+
+    nt = wall_now.toSec() - G_wall_begin.toSec();  // dt
+    positionr = (jointstate->position[0] - positionrb) /nt;  // 右車輪を微分してる　→車輪の回転速度
+    positionl = (jointstate->position[1] - positionlb) /nt;   // 左車輪を微分してる
+    
+    velr = positionr * 0.033 ;                   // 回転速度と半径をかけて車輪の進行速度を求めてる
+    vell = positionl * 0.033;
+    vel  = (velr + vell) / 2;                    //  車体の併進速度
+
+    W    = (vell - velr) / 0.158;                    // 車輪の進行速度で車体本体の回転速度を求めてる
+
+    s    = W * nt;                               // 車体のθを回転速度を微分して求めてる
+
+    Vx   = vel * sin(s);                         // 車体のx軸の速度に分解してる
+    Vy   = vel * cos(s);
+
+    X    = Vx * nt;                              // 位置
+    Y    = Vy * nt;
+
+    G_odom.pose.pose.position.x += X;
+    G_odom.pose.pose.position.y += Y;
+    G_wall_begin = wall_now;
+
+    G_publisher_odom.publish(G_odom);
+  }
+    
+    
+
+
+  
+  
+  
+  // 車軸の位置は積算される
+  ROS_INFO("Whell Pos (r:%f, l:%f)", wheel_right_joint_pos,wheel_left_joint_pos);
+}
+
+
+
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "my_odom3");
+  ros::NodeHandle nh;
+
+  //subscriberの作成。トピック/cmd_velを購読する。
+  ros::Subscriber sub  = nh.subscribe("/cmd_vel", 10, cbVel);
+  ros::Subscriber sub2 = nh.subscribe("/odom", 100, cbOdom);
+  ros::Subscriber sub3 = nh.subscribe("/gazebo/model_states", 100, cbModelStates);
+  ros::Subscriber sub4 = nh.subscribe("/joint_states", 100, cbMyOdom);
+  
+  G_publisher_odom = nh.advertise<nav_msgs::Odometry>("/dead_reckoning", 10);
+
+  
+  // コールバック関数を繰り返し呼び出す。
+  ros::Rate rate(100);
+
+  G_wall_begin = ros::Time::now();
+  ros::spin();
+
+  return 0;
+}
---
+++ src/planning.cpp
@@ -0,0 +1,176 @@
+/* -------------------------
+  FFのみの制御：動作計画
+  callbackを未使用
+  2022.4.8 by K. Hidaka
+-----------------------------*/
+#include "ros/ros.h"  // rosで必要なヘッダファイル
+#include <geometry_msgs/Twist.h> // ロボットを動かすために必要
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h> //初期位置関係
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_datatypes.h>
+#include <time.h> //計測時間
+#include "unistd.h"
+#include<iostream> //C++用入出力関係
+#include<fcntl.h> //ファイル読み書き用
+#include<math.h> //計算用
+#define DATA_FILE_PATH "./test_data.txt" //保存データパス設定
+using namespace std; // C++の命令 cout << に必要
+
+/*                       
+  ロボットの移動命令部分
+  sampleでは並進速度 = v_vel/回転速度 a_velを事前に設定
+*/
+int main(int argc, char **argv)
+{ 
+  /* ------------------------------
+            初期設定
+--------------------------------*/ 
+    ros::init(argc, argv, "my_robot_cont2");
+    ros::NodeHandle nh; // 送受信用
+    ros::Publisher pub;
+    geometry_msgs::Twist vel;
+    pub= nh.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
+
+
+    double linear_vel = 0.0; // 速度[m/s]
+    double angular_vel = 0.0; //回転速度[rad/s]
+    ros::WallDuration w_t_t; //WallTimeは壁時計時間=実時間のこと、w_t_tを宣言
+    
+    // 処理ループ周期 T = 1/10 = 0.1[s] <= 10[Hz]
+    ros::Rate rate(10);
+    
+
+
+    //ファイル読み込み用
+     FILE *fi;
+    /* ファイルのオープン 
+      データ保存場所を設定しておく．
+      ここでは /home/home/ubuntu/catkin_ws/の場所に
+      testd.txt　の名前で保存
+      w+ : 読み書き可能：上書きされる.
+      参考HP：https://programming.pc-note.net/c/file2.html
+     */
+      fi = fopen("/home/ubuntu/catkin_ws/testd.txt", "w+");
+        if(fi == NULL){
+          printf("file open error:input.txt\n");
+        return -1;
+        }
+
+
+
+    // 初期速度/走行時間
+       linear_vel = 0; // 速度[m/s]
+       angular_vel = 0; //回転速度[rad/s]
+       //ROS_INFO("moveSecond:begin V%d,Q%d", linear_vel, angular_vel);
+
+     /* 計測スタート */    
+        ros::WallTime wall_begin = ros::WallTime::now();
+         //クラス::壁時計で宣言、変数wall_biginに関数now()の値を代入
+  
+      /* 無限ループ:while{ros::ok())を利用して動作制御入力を発生
+         無限ループで送信する場合：ros::spinOnce();を利用して1回ごとに呼び出す */
+          vel.linear.x  = linear_vel; //並進速度命令
+          vel.angular.z = angular_vel; //回転速度命令
+          //時間設定
+          
+          ros::WallDuration first_t = ros::WallDuration(2.0);
+          ros::WallDuration second_t = ros::WallDuration(5.0);
+          ros::WallDuration third_t = ros::WallDuration(7.0);
+          ros::WallDuration fourth_t = ros::WallDuration(17.0);
+          ros::WallDuration fifth_t = ros::WallDuration(19.0);
+          ros::WallDuration sixth_t = ros::WallDuration(22.0);
+          ros::WallDuration p_f_t = ros::WallDuration(24.0);
+          
+          ros::WallDuration f_t = ros::WallDuration(30.0);
+
+          cout << "start_time %u \n" << f_t << endl;
+
+
+/* ここから制御ループ */          
+      while(ros::ok())
+      {
+          ros::WallTime wall_now = ros::WallTime::now();//時間計測と更新
+          w_t_t = wall_now - wall_begin; //経過時間を現在時刻 - 開始時刻と設定
+    /*  --------------------------------------------------
+        以下に動作計画用速度式を
+        プログラムする.  
+        ifを使用/swichを使用
+        参考HP：https://demura.net/education/16287.html
+        以下のprogramは一例です．完成版ではありません．
+      ---------------------------------------------------- */
+    
+        
+     if (w_t_t < first_t) //0秒から2秒(加速)
+      {
+        cout << "Time %u" << w_t_t << endl;
+        int t_t = w_t_t.sec;//t_tに経過時間の実数値を代入
+          vel.linear.x = 0.1*t_t; //加速の式
+          vel.angular.z = 0.0;
+        cout << "v1\n" << vel.linear.x << endl;
+      }
+      else if (first_t <= w_t_t && w_t_t < second_t)//2秒から5秒(定速)
+      {
+            cout << "Time %u" << w_t_t << endl;
+            vel.linear.x = 0.2;
+            vel.angular.z = 0.0;
+            cout << "v2\n" << vel.linear.x << endl;
+      }
+      else if (second_t <= w_t_t && w_t_t < third_t)//5秒から7秒(減速)
+      {
+            cout << "Time %u" << w_t_t << endl;
+            int t_t = w_t_t.sec;
+            vel.linear.x = -0.1*t_t+0.7;
+            vel.angular.z = 0.0;
+            cout << "v2\n" << vel.linear.x << endl;
+      }
+      else if (third_t <= w_t_t && w_t_t < fourth_t)//回転(7~17秒)
+      {
+            int t_t_2 = w_t_t.sec -7;
+            cout << "Time %u" << w_t_t << endl;
+            vel.linear.x = 0.0;
+            vel.angular.z = 0.1*M_PI;
+            cout << "v3\n" << vel.linear.x << endl;
+      }
+       else if (fourth_t <= w_t_t && w_t_t < fifth_t) //17秒から19秒(加速)
+      {
+        cout << "Time %u" << w_t_t << endl;
+        int t_t = w_t_t.sec - 17;//t_tに経過時間の実数値を代入
+          vel.linear.x = 0.1*t_t; //加速の式
+          vel.angular.z = 0.0;
+        cout << "v1\n" << vel.linear.x << endl;
+      }
+      else if (fifth_t <= w_t_t && w_t_t < sixth_t)//19秒から22秒(定速)
+      {
+            cout << "Time %u" << w_t_t << endl;
+            vel.linear.x = 0.2;
+            vel.angular.z = 0.0;
+            cout << "v2\n" << vel.linear.x << endl;
+      }
+      else if (sixth_t <= w_t_t && w_t_t < p_f_t)//22秒から24秒(減速)
+      {
+            cout << "Time %u" << w_t_t << endl;
+            int t_t = w_t_t.sec - 17;
+            vel.linear.x = -0.1*t_t+0.7;
+            vel.angular.z = 0.0;
+            cout << "v2\n" << vel.linear.x << endl;
+      }
+      else if (p_f_t <= w_t_t)
+      {
+        cout << "Time %u" << w_t_t << endl;
+            vel.linear.x = 0.0;
+            vel.angular.z = 0.0;
+            cout << "v2\n" << vel.linear.x << endl;
+      }
+
+
+      pub.publish(vel);
+       /* 制御入力データ保存 */
+      fprintf(fi,"Time data = %u.%u ", w_t_t.sec, w_t_t.nsec);
+      fprintf(fi,"V = %f, Q = %f \n", vel.linear.x, vel.angular.z);
+      ros::spinOnce();
+       rate.sleep();        // 指定した周期でループするよう寝て待つ 
+      }
+    fclose(fi);
+    return 0;
+}
